1fc111f8b4f81783b728324c097e8d6c2a4a1b78
VARMA KVM <86279110+varmakvm789@users.noreply.github.com>
2024-09-07 22:56:29 +0530

    Add files via upload

---
+++ Business Queries Netflix.sql
@@ -0,0 +1,22 @@
+-- Business Queries Netflix Project --
+
+
+1. How many movies and TV shows are in the dataset?
+2. Find the most common rating for movies and TV shows
+3. How many shows and movies were released in each year?
+4. Which countries have produced the most content on Netflix? 
+5. What are the top 5 most common genres in the dataset?
+6. Find the content added in the last 5 years ?
+7. What is the distribution of content by rating (e.g., PG, PG-13)?
+8. Who are the top 5 directors with the most content on Netflix?
+9. How many shows or movies have been released by each country every year?
+10. Find each year and the average numbers of content release in India on Netflix, return top 5 year with highest avg content release!
+11. What is the average duration of Netflix movies?
+12. What are the trends in content production over time for movies vs. TV shows?
+13. What are the most popular genres each year?
+14. Which genres are most popular in each country?
+15. When did Netflix experience the largest growth in content production?
+16. Group Netflix content based on release year, type, genre, and country
+17. Find the top 10 actors who have appeared in the highest number of movies produced in India.
+18. Categorize the content based on the presence of the keywords 'kill' and 'violence' in the description field. Label content containing these keywords as 'Bad' and all other 
+    content as 'Good'. Count how many items fall into each category.
