a63ff0c8d6c6da559065bb287c61f45216b7ee42
VARMA KVM <86279110+varmakvm789@users.noreply.github.com>
2024-09-07 23:15:07 +0530

    Solutions of Netflix.sql

--- Solutions of Netflix.sql
+++ Solutions of Netflix.sql
@@ -4,20 +4,20 @@ SELECT * FROM Netflix ;
 
 1. How many movies and TV shows are in the dataset?
 
-	  SELECT
+   SELECT
         type,
         COUNT(*) as content_count
-   FROM netflix 
+   FROM Netflix 
    GROUP BY type;
 
 2. Find the most common rating for movies and TV shows
 
-	WITH RatingSummary AS (
+WITH RatingSummary AS (
     SELECT
           type,
           rating,
           COUNT(*) AS rating_count 
-    FROM netflix 
+    FROM Netflix 
     GROUP BY type, rating
 ),
 RankedSummary AS (
@@ -37,7 +37,7 @@ WHERE dense_rank = 1;
 
 3. How many shows and movies were released in each year?
 
-	 SELECT
+   SELECT
         release_year,
         COUNT(*) AS total_content 
    FROM Netflix 
@@ -47,7 +47,7 @@ WHERE dense_rank = 1;
 
 4. Which countries have produced the most content on Netflix? 
 
-	SELECT
+   SELECT
         country,
         COUNT(*) AS total_content 
    FROM (
@@ -61,7 +61,7 @@ WHERE dense_rank = 1;
 
 5. What are the top 5 most common genres in the dataset?
 
-	SELECT
+   SELECT
         UNNEST(STRING_TO_ARRAY(listed_in, ',')) AS genre,
         COUNT(*) AS total_content 
    FROM Netflix 
@@ -72,27 +72,27 @@ WHERE dense_rank = 1;
 
 6. Find the content added in the last 5 years ?
 
-	 SELECT * 
-   FROM netflix 
+   SELECT * 
+   FROM Netflix 
    WHERE TO_DATE(date_added, 'Month DD, YYYY') >= CURRENT_DATE - INTERVAL '5 years';
 
 
 7. What is the distribution of content by rating (e.g., PG, PG-13)?
 
-	SELECT
+   SELECT
         rating,
         COUNT(*) AS total_content 
-   FROM netflix 
+   FROM Netflix 
    GROUP BY rating 
    ORDER BY total_content DESC;
 
 
 8. Who are the top 5 directors with the most content on Netflix?
 
-	SELECT
+   SELECT
         TRIM(UNNEST(STRING_TO_ARRAY(director, ','))) AS director,
         COUNT(*) AS total_content 
-   FROM netflix 
+   FROM Netflix 
    GROUP BY director 
    ORDER BY total_content DESC 
    LIMIT 5;
@@ -100,13 +100,13 @@ WHERE dense_rank = 1;
 
 9. How many shows or movies have been released by each country every year?
 
-	 SELECT
+   SELECT
         country,
         release_year,
         COUNT(*) AS total_content 
    FROM (
        SELECT TRIM(UNNEST(STRING_TO_ARRAY(country, ','))) AS country 
-       FROM netflix
+       FROM Netflix
    ) AS t 
    WHERE country IS NOT NULL 
    GROUP BY country, release_year 
@@ -115,11 +115,11 @@ WHERE dense_rank = 1;
 
 10. Find each year and the average numbers of content release in India on Netflix, return top 5 year with highest avg content release!
 
-	SELECT
+   SELECT
         release_year,
         COUNT(show_id) AS total_releases, 
         ROUND(AVG(COUNT(show_id)) OVER (PARTITION BY release_year), 2) AS avg_releases
-   FROM netflix 
+   FROM Netflix 
    WHERE country ILIKE '%India%' 
    GROUP BY release_year 
    ORDER BY avg_releases DESC 
@@ -128,48 +128,48 @@ WHERE dense_rank = 1;
 
 11. What is the average duration of Netflix movies?
 
-	 SELECT ROUND(AVG(SPLIT_PART(duration, ' ', 1)::INT), 2) AS avg_duration 
-   FROM netflix 
+   SELECT ROUND(AVG(SPLIT_PART(duration, ' ', 1)::INT), 2) AS avg_duration 
+   FROM Netflix 
    WHERE type = 'Movie';
 
 
 12. What are the trends in content production over time for movies vs. TV shows?
 
-	SELECT
+   SELECT
         release_year, type,
         COUNT(*) AS total_content 
-   FROM netflix 
+   FROM Netflix 
    GROUP BY release_year, type 
    ORDER BY release_year, type;
 
 
 13. What are the most popular genres each year?
 
-	   SELECT
+   SELECT
          release_year,
          UNNEST(STRING_TO_ARRAY(listed_in, ',')) AS genre, COUNT(*) AS total_content 
-   FROM netflix 
+   FROM Netflix 
    GROUP BY release_year, genre 
    ORDER BY release_year, total_content DESC;
 
 
 14. Which genres are most popular in each country?
 
-	   SELECT
+   SELECT
         country,
         UNNEST(STRING_TO_ARRAY(listed_in, ',')) AS genre,
         COUNT(*) AS total_content 
-   FROM netflix 
+   FROM Netflix 
    GROUP BY country, genre 
    ORDER BY country, total_content DESC;
 
 
 15. When did Netflix experience the largest growth in content production?
 
-	   SELECT
+   SELECT
         release_year,
         COUNT(*) AS total_content 
-   FROM netflix 
+   FROM Netflix 
    GROUP BY release_year 
    ORDER BY total_content DESC 
    LIMIT 1;
@@ -177,20 +177,20 @@ WHERE dense_rank = 1;
 
 16. Group Netflix content based on release year, type, genre, and country
 
-	 SELECT
+   SELECT
         release_year,
         type,
         UNNEST(STRING_TO_ARRAY(listed_in, ',')) AS genre,
         country 
-   FROM netflix;
+   FROM Netflix;
 
 
 17. Find the top 10 actors who have appeared in the highest number of movies produced in India.
 
-	   SELECT
+   SELECT
         UNNEST(STRING_TO_ARRAY(casts, ',')) AS actor,
         COUNT(*) AS total_movies 
-   FROM netflix 
+   FROM Netflix 
    WHERE country ILIKE '%India%' 
    GROUP BY actor 
    ORDER BY total_movies DESC 
@@ -200,13 +200,14 @@ WHERE dense_rank = 1;
 18. Categorize the content based on the presence of the keywords 'kill' and 'violence' in the description field. Label content containing these keywords as 'Bad' and all other 
     content as 'Good'. Count how many items fall into each category.
 
-	   SELECT category,
-          COUNT(*) AS content_count 
-   FROM (
+SELECT 
+    category,
+    COUNT(*) AS content_count 
+FROM (
        SELECT CASE 
                WHEN description ILIKE '%kill%' OR description ILIKE '%violence%' THEN 'Bad'
                ELSE 'Good' 
              END AS category 
-       FROM netflix
-   ) AS categorized_content 
-   GROUP BY category;
+       FROM Netflix
+     ) AS categorized_content 
+GROUP BY category;
